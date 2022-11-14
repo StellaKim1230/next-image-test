096980b91d16ccb8ee527ff4dd3606cbe5e3a665
StellaKim1230 <stellakim1230@gmail.com>
2022-11-14 23:26:03 +0900

    next/image study.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dev.present.do',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
